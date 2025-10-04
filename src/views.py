2acaa61ff0d9648417667ecc975385e878fda2de
Juana Solano <juana.solano@correo.tdea.edu.co>
2025-10-04 13:42:24 -0500

    redefine structure and install tailwind

--- src/views.py
+++ src/views.py
@@ -1,3 +1,4 @@
+from urllib import request
 from django.shortcuts import render
 
 # Create your views here.
@@ -5,3 +6,6 @@ from django.http import HttpResponse
 
 def inicio(request):
     return HttpResponse("¡Hola! Esta es la primera página web con Django.")
+
+def driver_registration(request):
+    return render(request, 'driver_registration.html')
