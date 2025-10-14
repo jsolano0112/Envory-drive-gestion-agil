2b923f0c57edc58e59977a2dd6a23b1965a6b24b
andersonVJ <andersonvalencia23j@gmail.com>
2025-10-14 18:07:18 -0500

    Login base creado

--- src/views.py
+++ src/views.py
@@ -1,11 +1,16 @@
-from urllib import request
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+
+@login_required
+def index(request):
+    return render(request, 'index.html')
 
 def inicio(request):
     return HttpResponse("¡Hola! Esta es la primera página web con Django.")
 
 def driver_registration(request):
     return render(request, 'driver_registration.html')
+
+def login(request):
+    return render(request, 'login.html')
